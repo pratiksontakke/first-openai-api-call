556c6765815e96c858ae79af583a32059c8be637
pratiksontakke <pratikass488@gmail.com>
2025-06-11 15:00:01 +0530

    update

--- first_call.py
+++ first_call.py
@@ -5,9 +5,28 @@ load_dotenv()
 
 client = OpenAI()
 
-response = client.responses.create(
-    model="gpt-4.1",
-    input="Write a one-sentence bedtime story about a unicorn."
-)
+system_prompt = "You are a helpful assistant."
+def chat_once(user_input: str):
+    response = client.responses.create(
+        model="gpt-3.5-turbo",
+        input=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_input}
+        ]
+    )
+    print("respo/nse :", response)
+    reply = response.output[0].content[0].text
+    usage = response.usage
 
-print(response)
+    print("\n=== Assistant Reply ===\n")
+    print(reply)
+
+    print("\n=== Token Usage ===")
+    print(f"Prompt Tokens     : {usage.input_tokens}")
+    print(f"Completion Tokens : {usage.output_tokens}")
+    print(f"Total Tokens      : {usage.total_tokens}")
+
+if __name__ == "__main__":
+    print("🔹 Chat with OpenAI 🔹")
+    user_prompt = input("\n👉 Enter your question: ")
+    chat_once(user_prompt)
